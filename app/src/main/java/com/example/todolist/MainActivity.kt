f9016608da8680e391294a02921c16dead72db56
bluebird-0 <dlsgkr5581@gmail.com>
2019-05-20 19:45:40 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.view.WindowManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar.setTitle("")
+        setSupportActionBar(toolbar)
+
+        val count = object : CountDownTimer(1000000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timer.setText(""+millisUntilFinished/60000+":"+millisUntilFinished%60000/1000)
+            }
+
+            override fun onFinish() {
+                timer.setText("done!");
+            }
+        }.start()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
